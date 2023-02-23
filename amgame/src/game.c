50521fc79f42a7a9c15b594aa232a104b1bf854b
tracer-nju <tracer@nju.edu.cn>
2023-02-23 22:38:36 +0800

    Linux longfar-virtual-machine 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:38:36 up 31 min,  1 user,  load average: 0.11, 0.14, 0.11

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,5 +1,47 @@
 #include <game.h>
 
+struct
+{
+  int x;
+  int y;
+  int w;
+  int h;
+  uint32_t color;
+} my_rec = {5,5,10,20, 0xFFFFFF};
+
+void draw_my_rec() {
+  draw_rectangle(my_rec.x, my_rec.y, my_rec.w, my_rec.h, my_rec.color);
+}
+
+void erase_my_rec() {
+  draw_rectangle(my_rec.x, my_rec.y, my_rec.w, my_rec.h, 0);
+}
+
+
+void move_up() {
+  erase_my_rec();
+  my_rec.y -= 1;
+  draw_my_rec();
+}
+
+void move_down() {
+  erase_my_rec();
+  my_rec.y += 1;
+  draw_my_rec();
+}
+
+void move_left() {
+  erase_my_rec();
+  my_rec.x -= 1;
+  draw_my_rec();
+}
+
+void move_right() {
+  erase_my_rec();
+  my_rec.x += 1;
+  draw_my_rec();
+}
+
 // Operating system is a C program!
 int main(const char *args) {
   ioe_init();
@@ -14,6 +56,15 @@ int main(const char *args) {
   puts("Press any key to see its key code...\n");
   while (1) {
     const char* key = print_key();
+    if (strcmp(key, "W") == 0) {
+      move_up();
+    } else if (strcmp(key, "S") == 0) {
+      move_down();
+    } else if (strcmp(key, "A") == 0) {
+      move_left();
+    } else if (strcmp(key, "D") == 0) {
+      move_right();
+    }
   }
   return 0;
 }
